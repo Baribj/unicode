9c657d5c62f5c23e088d30b7dbba9e4e4b303caf
Bari <bari.mube@gmail.com>
2024-10-15 16:00:49 +0300

    feat: add build process

---
+++ dist/server.js
@@ -0,0 +1,30 @@
+import express from "express";
+import { createServer } from "http";
+import { Server as SocketIOServer } from "socket.io";
+import next from "next";
+const dev = process.env.NODE_ENV !== "production";
+const app = next({ dev });
+const handle = app.getRequestHandler();
+app.prepare().then(() => {
+    const server = express();
+    const httpServer = createServer(server);
+    // Check if Socket.IO is already attached to prevent multiple instances
+    let io;
+    if (!("io" in httpServer)) {
+        io = new SocketIOServer(httpServer);
+        httpServer.io = io;
+        io.on("connection", (socket) => {
+            console.log("New client connected");
+            socket.on("disconnect", () => {
+                console.log("Client disconnected");
+            });
+        });
+    }
+    else {
+        io = httpServer["io"];
+    }
+    server.all("*", (req, res) => handle(req, res));
+    httpServer.listen(process.env.PORT, () => {
+        console.log("Server running on http://localhost:3000");
+    });
+});
